e547e87463d65f4a6874b1fa3d1d3062a035d815
Kelvin Rodrigues <81050267+LunarioTK@users.noreply.github.com>
2022-12-17 11:51:46 +0000

    Drop-downs

---
+++ lib/screens/home/settings_form.dart
@@ -0,0 +1,68 @@
+import 'package:brew_crew/shared/constants.dart';
+import 'package:flutter/material.dart';
+
+class SettingsForm extends StatefulWidget {
+  const SettingsForm({super.key});
+
+  @override
+  State<SettingsForm> createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+
+  // form values
+  String _currentName = '';
+  String _currentSugars = '0';
+  final int _currentStrength = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          const Text(
+            'Update your brew settings',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          const SizedBox(height: 20.0),
+          TextFormField(
+            decoration: textInputDecoration,
+            validator: ((val) => val!.isEmpty ? 'Please enter a name' : null),
+            onChanged: ((val) => setState(() => _currentName = val)),
+          ),
+          const SizedBox(height: 20.0),
+          //dropdow for sugars
+          DropdownButtonFormField(
+            decoration: textInputDecoration,
+            value: _currentSugars,
+            items: sugars.map((sugar) {
+              return DropdownMenuItem(
+                value: sugar,
+                child: Text('$sugar sugars'),
+              );
+            }).toList(),
+            onChanged: ((val) => setState(() => _currentSugars = val!)),
+          ),
+          //slider for strength
+          ElevatedButton(
+            onPressed: (() async {
+              print(_currentName);
+              print(_currentSugars);
+              print(_currentStrength);
+            }),
+            style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Colors.green),
+            ),
+            child: const Text(
+              'Update',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
